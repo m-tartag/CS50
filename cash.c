e7877265ccad3962ab052070e5c1b5a475d444bf
m-tartag <mattbrs51@gmail.com>
2019-08-05 10:42:03 -0400

    update

--- cash.c
+++ cash.c
@@ -6,11 +6,17 @@
 int main(void)
 {
     float input;
+    float penny, nickel, dime, quarter;
+
+    penny = 0.01;
+    nickel = 0.05;
+    dime = 0.10;
+    quarter = 0.25;
 
     do
     {
         input = get_float("%s", "Submit: \n");
         printf("%f", input);
         printf("\n");
-    } while (input < 0);
+    } while (input < 0)
 }
